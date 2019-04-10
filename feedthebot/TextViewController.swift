e4e5f816f14fdbf25abd50120dacc390726db57f
mobileflowllc <fieldforceapp@gmail.com>
2019-04-09 21:37:05 -0700

    Add text training

---
+++ feedthebot/TextViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TextViewController.swift
+//  feedthebot
+//
+//  Created by d. nye on 4/9/19.
+//  Copyright © 2019 Mobile Flow LLC. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+    @IBAction func doDoneButton(_ sender: Any) {
+        self.navigationController?.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func doSkipButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- feedthebot/TrainViewController.swift
+++ feedthebot/TrainViewController.swift
@@ -30,36 +30,51 @@ class TrainViewController: UIViewController {
         
     }
 
+    func showTrainingController(_ identifier: String) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: identifier) as! UINavigationController
+        self.present(vc, animated: true) {
+            //            print("Show login...")
+        }
+
+    }
+    
+    
     func setupStatButtons() {
         
         var buttons = [MFTrainButton]()
         let buttonShot = MFTrainButton(title: "TEXT", icon: "icon_text")
         buttonShot.completionHandler = { (sender) in
             print(sender)
+            self.showTrainingController("TEXTTRAINING")
         }
         buttons.append(buttonShot)
         
         let buttonCorner = MFTrainButton(title: "BOUNDING BOX", icon: "icon_bounding")
         buttonCorner.completionHandler = { (sender) in
             print(sender)
+            self.showTrainingController("TEXTTRAINING")
         }
         buttons.append(buttonCorner)
         
         let buttonGoal = MFTrainButton(title: "POLYGON LABEL", icon: "icon_polygon")
         buttonGoal.completionHandler = { (sender) in
             print(sender)
+            self.showTrainingController("TEXTTRAINING")
         }
         buttons.append(buttonGoal)
         
         let buttonTurnover = MFTrainButton(title: "CLASSIFICATION", icon: "icon_classify")
         buttonTurnover.completionHandler = { (sender) in
             print(sender)
+            self.showTrainingController("TEXTTRAINING")
         }
         buttons.append(buttonTurnover)
         
         let buttonOff = MFTrainButton(title: "PIXEL By PIXEL", icon: "icon_pixelwise")
         buttonOff.completionHandler = { (sender) in
             print(sender)
+            self.showTrainingController("TEXTTRAINING")
         }
         buttons.append(buttonOff)
                 
